aa83b89b1e40a945e5d7d03f272dfd1bbf348898
Ken Steckler <stecklek@oregonstate.edu>
2022-04-04 21:14:27 -0700

    update

--- src/Components/Works.js
+++ src/Components/Works.js
@@ -1,8 +1,5 @@
 import React from "react";
 import "../dist/css/Work.css";
-import photography from "./images/photography.png";
-import weatherly from "./images/weatherly.png";
-import Kohee from "./images/Kohee.png";
 
 function Works(props) {
   return (
@@ -13,27 +10,14 @@ function Works(props) {
         </div>
         <div className="work-cards">
           <div className="work-card">
-            <h2>Kohee Coffee e-Commerce Site</h2>
-            <p>Technology: REACT, CSS/SASS</p>
+            <h2>Giftxchange</h2>
+            <p>Technology: EJS, JavaScript, Express, Bootstrap</p>
             <p>Work in progress</p>
-            <img src={Kohee}></img>
           </div>
           <div className="work-card">
             <h2>Weatherly: Simply a weather app</h2>
-            <p>Technology: REACT, NODE, API, JSON</p>
+            <p>Technology: ReactJS, NodeJS, API, JSON</p>
             <a href="https://ken-steckler.github.io/weatherly/">Link</a>
-            <img src={weatherly}></img>
-          </div>
-          <div className="work-card">
-            <h2>Photography Portfolio</h2>
-            <p>Technology: Squarespace</p>
-            <a href="https://www.kenstecklerphotography.com">Link</a>
-            <img src={photography}></img>
-          </div>
-          <div className="work-card">
-            <h2>Vaccine Scheduler</h2>
-            <p>Technology: HTML, CSS/SASS, NODE, API</p>
-            <p>Work in progress</p>
           </div>
         </div>
       </div>
